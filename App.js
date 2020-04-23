7aaf65358a0c7e82723b7054caedcd1320ea794b
Chachi Desuasido <engr.chachi@gmail.com>
2020-04-23 12:06:41 +0800

    Initial React-Native Template

--- App.js
+++ App.js
@@ -1,19 +1,97 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import 'react-native-gesture-handler';
+import React, {useState, useEffect} from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {StyleSheet, Text, View, StatusBar} from 'react-native';
+import {DefaultTheme, Provider as PaperProvider, Button, BottomNavigation} from 'react-native-paper';
+import HomeScreen from './screens/HomeScreen';
+import ListScreen from './screens/ListScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+const HomeStack = createStackNavigator();
+const ListStack = createStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+const ListStackScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <ListStack.Navigator screenOptions={topNavList}>
+      <ListStack.Screen name="List" component={ListScreen}/>
+    </ListStack.Navigator>
+
   );
 }
+const HomeStackScreen = () => {
+  return (
+    <HomeStack.Navigator screenOptions={topNavHome}>
+      <HomeStack.Screen name="Home" component={HomeScreen}/>
+    </HomeStack.Navigator>
+  )
+}
+const App = () => {
+  return (
+    <PaperProvider theme={theme}>
+      <StatusBar/>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Home" 
+        initialRouteName="Home"
+        activeColor="#f0edf6"
+        inactiveColor="#3e2465"
+        barStyle={{ backgroundColor: '#000000' }}>
+
+          <Tab.Screen
+            name="Home"
+            component={HomeStackScreen}
+            options={{ 
+            tabBarIcon: ({color}) => (<Ionicons name="ios-home" color={color} size={26}/>)
+          }}/>
+          <Tab.Screen
+            name="Lists"
+            component={ListStackScreen}
+            options={{ 
+            tabBarIcon: ({color}) => (<Ionicons name="ios-notifications" color={color} size={26}/>)
+          }}/>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
+const topNavHome = {
+  headerStyle: {
+    backgroundColor: '#18191A'
+  },
+  headerTintColor: '#fff',
+  headerTintStyle: {
+    fontWeight: 'bold'
+  },
+  gestureEnabled: true
+}
+const topNavList = {
+  headerStyle: {
+    backgroundColor: '#18191A'
+  },
+  headerTintColor: '#fff',
+  headerTintStyle: {
+    fontWeight: 'bold'
+  },
+  gestureEnabled: true
+}
+const theme = {
+  ...DefaultTheme,
+  dark: 'true',
+  mode: 'exact',
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow'
+  }
+};
+export default App;
---
+++ screens/ListScreen.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from 'react'
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
+import {List} from 'react-native-paper';
+import {DataTable} from 'react-native-data-table'
+const ListScreen = () => {
+ 
+  return (
+    <ScrollView contentContainerStyle={styles.contentContainer}>
+   
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingVertical: 20
+  }
+});
+export default ListScreen;
